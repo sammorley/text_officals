c27d82b716db7a7a65bd199bf3d139914684678c
Samuel Morley <samuelmorley@live.com>
2016-11-06 20:17:32 -0800

    Update no_dapl.py

--- no_dapl.py
+++ no_dapl.py
@@ -5,10 +5,10 @@
 import android
 
 droid = android.Android()
-data = open('/storage/external_SD/Download/nd_houseroster.txt')
-dyde = droid.dialogGetInput("Text to public officials","type your message here","").result
+data = open('/storage/external_SD/Download/nd_officals.txt')
+text = droid.dialogGetInput("Text to public officials","type your message here","").result
 
 for line in data:
     line = line.replace('\n','')
-    droid.smsSend( (line),dyde)
+    droid.smsSend( (line),text)
 data.close()
